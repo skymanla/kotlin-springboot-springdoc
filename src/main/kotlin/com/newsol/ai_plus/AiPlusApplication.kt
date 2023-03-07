81d2e2b81d94b53eb922a281a0caea2edeba957b
김남태 <ryan@thebiz.kr>
2023-03-07 17:59:28 +0900

    init

---
+++ src/main/kotlin/com/newsol/ai_plus/AiPlusApplication.kt
@@ -0,0 +1,11 @@
+package com.newsol.ai_plus
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AiPlusApplication
+
+fun main(args: Array<String>) {
+	runApplication<AiPlusApplication>(*args)
+}
---
+++ src/test/kotlin/com/newsol/ai_plus/AiPlusApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.newsol.ai_plus
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AiPlusApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
